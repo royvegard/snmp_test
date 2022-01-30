430d03866f402f75dd4e98f18c2d74d70602e0c5
Roy Vegard Ovesen <roy.vegard.ovesen@gmail.com>
2022-01-30 14:27:07 +0100

    Rename variable, timeout, snmp-variables

--- Program.cs
+++ Program.cs
@@ -9,14 +9,14 @@ namespace snmp_test
 {
     class Program
     {
-        static int fetches = 1;
+        static int threads = 1;
         static async Task Main(string[] args)
         {
             if (args.Length > 0)
             {
-                fetches = Convert.ToInt32(args[0]);
+                threads = Convert.ToInt32(args[0]);
             }
-            System.Console.WriteLine("{0}", fetches);
+            System.Console.WriteLine("Threads: {0}", threads);
 
             Progress<int> progress = new Progress<int>();
             progress.ProgressChanged += ReportProgress;
@@ -33,9 +33,9 @@ namespace snmp_test
             List<Task<IList<Variable>>> tasks = new List<Task<IList<Variable>>>();
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            for (int i = 0; i < fetches; i++)
+            for (int i = 0; i < threads; i++)
             {
-                tasks.Add(Task.Run(() => Messenger.Get(VersionCode.V1,
+                tasks.Add(Task.Run(() => Messenger.Get(VersionCode.V2,
                 new IPEndPoint(IPAddress.Parse("35.157.118.224"), 161),
                 new OctetString("public"),
                 new List<Variable> {
@@ -45,27 +45,20 @@ namespace snmp_test
                     new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.6.0")),
                     new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.7.0")),
                     new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.9.1.3.1")),
+                    new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.9.1.3.2")),
+                    new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.9.1.3.3")),
                     new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.9.1.3.4")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.9.1.3.6")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.9.1.3.8")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.1.1")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.1.2")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.2.1")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.2.2")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.3.1")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.3.2")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.4.1")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.4.2")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.5.1")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.5.2")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.6.1")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.6.2")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.7.1")),
-                    // new Variable(new ObjectIdentifier("1.3.6.1.2.1.2.2.1.7.2")),
+                    new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.9.1.3.5")),
+                    new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.9.1.3.6")),
+                    new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.9.1.3.7")),
+                    new Variable(new ObjectIdentifier("1.3.6.1.2.1.1.9.1.3.8")),
+                    new Variable(new ObjectIdentifier("1.3.6.1.2.1.25.1.1.0")),
+                    new Variable(new ObjectIdentifier("1.3.6.1.2.1.25.1.4.0")),
+                    new Variable(new ObjectIdentifier("1.3.6.1.2.1.25.1.6.0")),
                 },
-                60000)));
+                1000)));
 
-                progress.Report((i * 100) / fetches);
+                progress.Report((i * 100) / threads);
             }
 
             var results = await Task.WhenAll(tasks);
@@ -75,8 +68,8 @@ namespace snmp_test
             System.Console.WriteLine();
 
             System.Console.WriteLine("Run time: {0} ms", watch.ElapsedMilliseconds);
-            System.Console.WriteLine("Variables fetched: {0}", (double)(fetches * results[0].Count));
-            System.Console.WriteLine("Variable fetch rate: {0} ms/variable", (double)watch.ElapsedMilliseconds / (double)(fetches * results[0].Count));
+            System.Console.WriteLine("Variables fetched: {0}", (double)(threads * results[0].Count));
+            System.Console.WriteLine("Variable fetch rate: {0} ms/variable", (double)watch.ElapsedMilliseconds / (double)(threads * results[0].Count));
         }
     }
 }
